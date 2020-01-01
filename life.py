0e0e413fc6ef025eeb5ca9ca6f1720b4963a71be
shireensharaf <shirenmsharaf@gmail.com>
2020-01-01 13:19:34 +0530

    grid view -  refactor

--- life.py
+++ life.py
@@ -1,2 +1,13 @@
 def grid_view(rows,cols, l):
-    return [['-', '-', '-'],['*', '-', '*'],['-', '-', '-']]
+    col_count = 0
+    l2 =[]
+    for i in range(0,rows):
+        l1 = []
+        for j in range(0, cols):
+            if(col_count in l):
+                l1.append('*')
+            else:
+                l1.append('-')
+            col_count += 1
+        l2.append(l1)
+    return l2
--- test_life.py
+++ test_life.py
@@ -4,3 +4,7 @@ def test_life_grid_2_true():
     expected_value = [['-', '-', '-'],['*', '-', '*'],['-', '-', '-']]
     assert life.grid_view(3,3,[3,5]) == expected_value
 
+def test_life_grid_3rows_4cols():
+    expected_value = [['-', '*', '-','*'],[ '-', '*','-', '-'],[ '-','-', '-', '-']]
+    assert life.grid_view(3,4,[1,3,5]) == expected_value
+
