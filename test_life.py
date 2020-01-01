2e1ad57193e2803db7399da89e611b2993fd6a5d
shireensharaf <shirenmsharaf@gmail.com>
2020-01-01 12:32:16 +0530

    grid view -  green

--- test_life.py
+++ test_life.py
@@ -2,6 +2,6 @@ import life
 
 def test_life_grid():
     expected_value = [['-', '-', '-'],['*', '*', '*'],['-', '-', '-']]
-    assert life.grid([[False, False, False],
+    assert life.grid_view([[False, False, False],
                      [True, False, True],
                       [False, False, False]]) == expected_value
